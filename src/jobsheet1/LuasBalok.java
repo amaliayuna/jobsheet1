feccc6c226263739274d6017234cfecee2bccb60
Amalia Yuna <Amalia Yuna@Amaliaaa>
2018-08-20 12:47:20 +0700

    commit keseluruhan

---
+++ src/jobsheet1/LuasBalok.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet1;
+
+/**
+ *
+ * @author Amalia Yuna
+ */
+public class LuasBalok {
+    public static void main(String[] args){
+        int p = 5;
+        int l = 10;
+        int t = 5;
+        
+    }
+    
+}
---
+++ src/jobsheet1/hellotelkom.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet1;
+
+/**
+ *
+ * @author Amalia Yuna
+ */
+public class hellotelkom {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("selamat belajar di program java");
+    }
+}
+
